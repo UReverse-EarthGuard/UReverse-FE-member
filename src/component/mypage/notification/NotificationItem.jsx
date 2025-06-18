5300768c723c6e47df3081a5d978d46c5145ad24
pch8349 <chan449@naver.com>
2025-06-18 18:13:50 +0900

    feat : 알림창 시간 데이터 출력

--- src/component/mypage/notification/NotificationItem.jsx
+++ src/component/mypage/notification/NotificationItem.jsx
@@ -1,12 +1,22 @@
+import { useEffect } from 'react';
 import styles from './NotificationItem.module.css';
 
 export default function NotificationItem({ data }) {
+    useEffect(() => {
+        console.log(data);
+    }, [])
 
     return (
         <div className={styles.card}>
-            <div className={`${styles.title} ${data.isRead === "N" ? styles.unread : ''}`}>
-                {data.title}
+            <div className={styles.titleRow}>
+                <div className={`${styles.title} ${data.isRead === "N" ? styles.unread : ''}`}>
+                    {data.title}
+                </div>
+                <div className={styles.time}>
+                    {data.createdAt?.slice(0, 16).replace('T', ' ') || '시간 정보 없음'}
+                </div>
             </div>
+            
             <div className={styles.message}>{data.message}</div>
         </div>
     );
